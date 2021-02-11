765e406080e3ea1edadd610b684967f39c73b285
StephWood <60517001+StephWood@users.noreply.github.com>
2021-02-10 20:59:20 -0800

    added redux connect to header and app components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -8,42 +9,49 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
+  /*
   constructor() {
     super();
 
     this.state = {
-      currentUser: null,
+      currentUser: null
     };
   }
+  */
 
   // set property for unsubscribe with default value of null
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     // firebase realizes that authentication state has changed, we want to be aware
     // open messaging system that indicates when auth state changes as long as component is mounted on the DOM, therefore need to also close the subscription on unmount to prevent memory leaks so we set up unsubscribeFromAuth
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot((snapshot) => {
-          this.setState(
-            {
-              currentUser: {
-                id: snapshot.id,
-                ...snapshot.data(),
-              },
-            }, // can't log data after async setState call, so pass func as second parameter to call it after state has fully propogated
-            () => {
-              console.log(this.state);
-            }
-          );
-          console.log(this.state);
-        });
+        userRef.onSnapshot(
+          (snapshot) => {
+            // this.setState(
+            //   {
+            //     currentUser: {
+            setCurrentUser({
+              id: snapshot.id,
+              ...snapshot.data(),
+            });
+          }, // can't log data after async setState call, so pass func as second parameter to call it after state has fully propogated
+          () => {
+            console.log(this.state);
+          }
+        );
+        console.log(this.state);
       }
-      this.setState({ currentUser: userAuth });
+      // this.setState({ currentUser: userAuth })
+      setCurrentUser(userAuth);
     });
   }
 
@@ -54,7 +62,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -65,4 +73,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
